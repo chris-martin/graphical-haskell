70cd939d5a1f0afac2e9f318ce1fa25623b15a3b
Chris Martin <ch.martin@gmail.com>
2019-11-23 19:13:12 -0700

    support splitting up imports

--- run-graphical-haskell.hs
+++ run-graphical-haskell.hs
@@ -1,32 +1,45 @@
 #!/usr/bin/env stack
--- stack --resolver lts-14.15 runhaskell --package xeno --package temporary --package bytestring
+-- stack --resolver lts-14.15 runhaskell --package xeno --package temporary --package bytestring --package text --package process
 
 import Control.Exception (throw)
 import Data.Foldable (traverse_)
 import System.Environment (getArgs)
 import System.IO.Temp (withSystemTempFile)
-import System.Process (callProcess)
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Builder as BSB
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy.Char8 as LBS8
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LT
 import qualified Xeno.DOM as X
 import qualified System.IO as IO
+import qualified System.Process as Proc
 
 main :: IO ()
 main =
     getArgs >>= \(svgPath : svgArgs) ->
     BS.readFile svgPath >>= \bs ->
     either throw return (X.parse bs) >>= \svgRoot ->
-    withSystemTempFile "graphical-haskell-program-.hs" $ \hsPath hsHandle ->
+    withSystemTempFile "imports-.hs" $ \importsPath importsHandle ->
+    withSystemTempFile "declarations-.hs" $ \declsPath declsHandle ->
+    withSystemTempFile "graphical-haskell-.hs" $ \hsPath hsHandle ->
       (
-        traverse_ (writeCode hsHandle) (nodeCodeBlocks svgRoot) >>
         IO.hClose hsHandle >>
-        callProcess "runhaskell" (hsPath : svgArgs)
+        traverse_ (writeCode importsHandle declsHandle) (nodeCodeBlocks svgRoot) >>
+        IO.hClose importsHandle >>
+        IO.hClose declsHandle >>
+        Proc.callCommand (unwords ["cat", importsPath, declsPath, ">", hsPath]) >>
+        Proc.callProcess "runhaskell" (hsPath : svgArgs)
       )
+
+-- Write a block of code to the appropriate handle.
+writeCode :: IO.Handle -> IO.Handle -> LBS.ByteString -> IO ()
+writeCode importsHandle declsHandle code =
+    let h = if LBS.isPrefixOf (LBS8.pack "import ") code
+            then importsHandle else declsHandle
+    in LBS.hPut h code >> IO.hPutStrLn h ""
 
 -- Find all SVG text nodes and get the text from each.
 nodeCodeBlocks :: X.Node -> [LBS.ByteString]
@@ -56,7 +69,3 @@ unescapeEntitiesLBS = LT.encodeUtf8 . unescapeEntitiesLT . LT.decodeUtf8
 unescapeEntitiesLT :: LT.Text -> LT.Text
 unescapeEntitiesLT = let r a b = LT.replace (LT.pack a) (LT.pack b) in
     r "&amp;" "&" . r "&lt;" "<" . r "&gt;" ">" . r "&quot;" "\"" . r "&apos;" "'"
-
--- Write a block of code to the handle.
-writeCode :: IO.Handle -> LBS.ByteString -> IO ()
-writeCode h code = LBS.hPut h code >> IO.hPutStrLn h ""
